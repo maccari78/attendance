5f480520ec4f96f6959e787c677f04a3042bf949
Danilo Maccari <maccari78@gmail.com>
2021-08-16 06:02:28 -0300

    Added remote mysql connection details

--- db/conn.php
+++ db/conn.php
@@ -1,10 +1,17 @@
 <?php
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+    // Development connection
+    // $host = '127.0.0.1';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';
 
+    // Remote database connection
+    $host = 'remotemysql.com';
+    $db = 'gdCuaxXNXP';
+    $user = 'gdCuaxXNXP';
+    $pass = 'pgYItifHwu';
+    $charset = 'utf8mb4';
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
     try{
